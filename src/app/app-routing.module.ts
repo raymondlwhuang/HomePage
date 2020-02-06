853bf6b0cb453ee7cab46ea47b93cda4bd218f7d
raymondlwhuang <raymondlwhuang@yahoo.com>
2020-02-06 09:27:33 -0500

    first back up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AnimalsComponent } from './animals/animals.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'animals', component: AnimalsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
