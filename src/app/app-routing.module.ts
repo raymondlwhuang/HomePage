270f27062a884630c8ec446e3c40dff80a345f75
raymondlwhuang <raymondlwhuang@yahoo.com>
2020-02-07 13:50:49 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AnimalsComponent } from './animals/animals.component';
+import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
+import { EmploymentComponent } from './employment/employment.component';
+import { EducationComponent } from './education/education.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'animals', component: AnimalsComponent},
+  { 
+    path: '', 
+    component: HomeComponent,
+    children : [
+        { path: 'employment', component: EmploymentComponent},
+        { path: 'education', component: EducationComponent},
+        { path: 'detail', component: AnimalDetailComponent}
+    ]
+  },
+  { path: 'animals', component: AnimalsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AnimalsComponent } from './animals/animals.component';
+import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
+import { EducationComponent } from './education/education.component';
+import { EmploymentComponent } from './employment/employment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AnimalsComponent
+    AnimalsComponent,
+    AnimalDetailComponent,
+    EducationComponent,
+    EmploymentComponent
   ],
   imports: [
     BrowserModule,
