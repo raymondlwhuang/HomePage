06de9b048854c9ed51bb0e6c5a1adbcb4cf739d9
raymondlwhuang <raymondlwhuang@yahoo.com>
2020-02-09 15:15:16 -0500

    update

--- src/app/employment/employment.component.ts
+++ src/app/employment/employment.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employment.component.css']
 })
 export class EmploymentComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
+    let slideIndex = 0;
+    
+    (function showSlides() {
+      let slides = $(".mySlides");
+      slides.css('display','none');
+      slideIndex = slideIndex >= slides.length ? 1 : slideIndex + 1;
+      $(slides[slideIndex-1]).fadeIn('slow');
+      setTimeout(showSlides, 5000);
+    })()
+ 
   }
 
 }
